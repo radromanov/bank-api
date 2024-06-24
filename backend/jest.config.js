19b55f30cf9545c4907b2f438349a7fd334b6983
Radostin Romanov <rad.romanov@gmail.com>
2024-06-24 11:46:08 +0300

    add: rimraf, concurrently, jest, nodemon, package.json scripts; update: tsconfig.json

---
+++ backend/jest.config.js
@@ -0,0 +1,18 @@
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**/*.ts"],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  moduleNameMapper: {
+    "@domain/(.*)": "<rootDir>/src/domain/$1",
+    "@api/(.*)": "<rootDir>/src/api/$1",
+    "@application/(.*)": "<rootDir>/src/application/$1",
+    "@infrastructure/(.*)": "<rootDir>/src/infrastructure/$1",
+    "@utils/(.*)": "<rootDir>/src/utils/$1",
+    "@main/(.*)": "<rootDir>/src/main/$1",
+    "@tests/(.*)": "<rootDir>/src/tests/$1",
+  },
+  roots: ["<rootDir>/tests/"],
+};
+
+module.exports = config;
