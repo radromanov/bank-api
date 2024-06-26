ce4bf615158d005bdc418107cdfc9f22f4c84491
Radostin Romanov <rad.romanov@gmail.com>
2024-06-26 14:58:34 +0300

    refactor: BankApiConfig uses zod instead of env-var

--- backend/src/config/bank-api.config.ts
+++ backend/src/config/bank-api.config.ts
@@ -1,15 +1,41 @@
+import z, { ZodError } from "zod";
 import "@shared/utils/dotenv";
-import { get } from "env-var";
+import { ApiError } from "@shared/utils/api-error";
 
 export class BankApiConfig {
+  private static schema = z.object({
+    port: z
+      .string({
+        required_error: "process.env.BANK_API_PORT is required",
+      })
+      .min(1, "process.env.BANK_API_PORT must contain at least 1 character(s)")
+      .transform((data) => parseInt(data, 10)),
+    host: z
+      .string({
+        required_error: "process.env.BANK_API_HOST is required",
+      })
+      .min(1, "process.env.BANK_API_HOST must contain at least 1 character(s)"),
+    env: z.enum(["development", "production", "staging", "testing"], {
+      required_error: "process.env.NODE_ENV is required",
+      invalid_type_error:
+        "Invalid process.env.NODE_ENV value. Expected 'development' | 'production' | 'staging' | 'testing'",
+    }),
+  });
+
   private static init() {
-    return {
-      port: get("BANK_API_PORT").required().asPortNumber(),
-      host: get("BANK_API_HOST").required().asString(),
-      env: get("NODE_ENV")
-        .required()
-        .asEnum(["development", "production", "staging", "testing"]),
-    };
+    try {
+      return this.schema.parse({
+        port: process.env.BANK_API_PORT,
+        host: process.env.BANK_API_HOST,
+        env: process.env.NODE_ENV,
+      });
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw ApiError.SERVICE_UNAVAILABLE(error.errors[0].message);
+      } else {
+        throw ApiError.INTERNAL_SERVER_ERROR();
+      }
+    }
   }
 
   static get() {
