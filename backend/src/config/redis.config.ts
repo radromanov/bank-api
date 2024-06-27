b99d77047e7eedf6177bfbed4bc87df1abead64c
Radostin Romanov <rad.romanov@gmail.com>
2024-06-27 11:41:47 +0300

    chore: update docker-compose.yml, redis.config, add redis.client methods

--- backend/src/config/redis.config.ts
+++ backend/src/config/redis.config.ts
@@ -4,20 +4,36 @@ import z, { ZodError } from "zod";
 
 export class RedisConfig {
   private static schema = z.object({
+    host: z
+      .string(errors("process.env.REDIS_HOST"))
+      .min(1, minimum("process.env.REDIS_HOST")),
     port: z
       .string(errors("process.env.REDIS_PORT"))
       .min(1, minimum("process.env.REDIS_PORT"))
       .transform((data) => parseInt(data, 10)),
-    dataPath: z
-      .string(errors("process.env.REDIS_DOCKER_VOLUME_PATH"))
-      .min(1, minimum("process.env.REDIS_DOCKER_VOLUME_PATH")),
+    user: z
+      .string(errors("process.env.REDIS_USER"))
+      .min(1, minimum("process.env.REDIS_USER")),
+    pass: z
+      .string(errors("process.env.REDIS_PASS"))
+      .min(1, minimum("process.env.REDIS_PASS")),
+    name: z
+      .string(errors("process.env.REDIS_NAME"))
+      .min(1, minimum("process.env.REDIS_NAME")),
+    url: z
+      .string(errors("process.env.REDIS_URL"))
+      .min(1, minimum("process.env.REDIS_URL")),
   });
 
   private static init() {
     try {
       return this.schema.parse({
+        host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT,
-        dataPath: process.env.REDIS_DOCKER_VOLUME_PATH,
+        user: process.env.REDIS_USER,
+        pass: process.env.REDIS_PASS,
+        name: process.env.REDIS_NAME,
+        url: process.env.REDIS_URL,
       });
     } catch (error) {
       if (error instanceof ZodError) {
--- backend/src/infrastructure/cache/redis/redis.client.ts
+++ backend/src/infrastructure/cache/redis/redis.client.ts
@@ -1,14 +1,59 @@
 import { RedisConfig } from "@config/redis.config";
 import { CacheClient } from "../cache.client";
+import { Redis } from "ioredis";
 
 export class RedisClient implements CacheClient {
-  private port: number;
-  private dataPath: string;
+  private _client: Redis | null;
+  private url: string;
 
   constructor(readonly redisConfig: typeof RedisConfig) {
-    const { port, dataPath } = redisConfig.get();
+    this.url = redisConfig.getOne("url");
 
-    this.port = port;
-    this.dataPath = dataPath;
+    this._client = this.init();
+  }
+
+  private init() {
+    if (!this._client) {
+      this._client = new Redis(this.url);
+    }
+
+    return this._client;
+  }
+
+  private get client() {
+    if (!this._client) {
+      this.init();
+    }
+
+    return this._client!;
+  }
+
+  async get<T>(key: string) {
+    const result = await this.client.get(key);
+    try {
+      if (!result) return null;
+
+      return JSON.parse(result) as T;
+    } catch (error) {
+      return result;
+    }
+  }
+
+  /**
+   * @param key Key to the key-value pair in Redis.
+   * @param data Data we're caching.
+   * @param expire Expiration time in seconds. Defaults to 900 seconds (15 minutes).
+   * @returns The key to the key-value pair.
+   */
+  async set(key: string, data: unknown, expire = 900) {
+    const value = typeof data === "string" ? data : JSON.stringify(data);
+
+    await this.client.multi().set(key, value).expire(key, expire).exec();
+
+    return key;
+  }
+
+  async del(key: string | string[]) {
+    await this.client.del(...key);
   }
 }
